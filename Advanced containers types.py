0ce3d5de0f4e364d714d2b99addbe841e9831c66
Kamale Akhileswar <159161616+Akhileswar6@users.noreply.github.com>
2025-01-31 21:21:56 +0530

    Update Advanced containers types.py

--- Advanced containers types.py
+++ Advanced containers types.py
@@ -173,14 +173,6 @@ print(nums.values())
 #getting all the items (key,value pair)
 nums={1:"one",2:"two",3:"three",8:"eight"}
 print(nums.items())
-
-
-
-
-
-a=6
-b="akhil"
-print(str(a)+b)
-
+print("AKhil")
 
 
